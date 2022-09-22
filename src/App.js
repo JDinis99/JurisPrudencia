82364207d36015d80f9d10c13bf30cdadbecd742
JDinis <joao.t.dinis@gmail.com>
2022-09-22 03:14:57 -0700

    new routes

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
         <div className="content">
           <Header/>
           <Routes>
-            <Route exact path="/" element={<ImportPage />}/>
-            <Route exact path="/anom" element={<Anom />}/>
-            <Route exact path="/ajuda" element={<HelpPage />}/>
+            <Route exact path="/anon" element={<ImportPage />}/>
+            <Route exact path="/anon/anom" element={<Anom />}/>
+            <Route exact path="/anon/ajuda" element={<HelpPage />}/>
           </Routes>
         </div>
       </div>
